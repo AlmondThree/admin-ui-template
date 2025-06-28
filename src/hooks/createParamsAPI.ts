b81a2a5da47a2fc9da5647f9f16c20d5cce4959f
AlmondThree <donnyfadhillah23@gmail.com>
2025-06-28 01:55:25 +0000

    MRG: APS-43

---
+++ src/hooks/createParamsAPI.ts
@@ -0,0 +1,13 @@
+const createParamsAPI = (paramsList:Array<string>, paramsValue:any) => {
+    let parameter = (paramsValue != null) ? "?" : "";
+    for(let i of paramsList) {
+        if (paramsValue[i] != null) {
+            parameter=parameter+`${i}=${paramsValue[i]}&`
+        }
+    }
+    const parameterFinal = parameter.slice(0, -1)
+
+    return parameterFinal
+}
+
+export default createParamsAPI
